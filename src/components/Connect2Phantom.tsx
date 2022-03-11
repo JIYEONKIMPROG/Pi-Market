43dbcf6a819d6b0b4408ba995d8d6e51570bc66e
JIYEONKIMPROG <88828307+JIYEONKIMPROG@users.noreply.github.com>
2022-03-12 04:41:39 +0900

    new version
    1. 프론트 html/css -> react, typescript
    2. 지갑 연결(계속 수정 예정)

---
+++ src/components/Connect2Phantom.tsx
@@ -0,0 +1,109 @@
+
+import { FC, useEffect, useState } from "react";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { useMediaQuery } from 'react-responsive';
+
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+
+interface ConnectOpts {
+    onlyIfTrusted: boolean;
+}
+
+interface PhantomProvider {
+    connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+    disconnect: ()=>Promise<void>;
+    on: (event: PhantomEvent, callback: (args:any)=>void) => void;
+    isPhantom: boolean;
+}
+
+type WindowWithSolana = Window & { 
+    solana?: PhantomProvider;
+}
+
+
+
+const Connect2Phantom: FC = () => {
+
+    const [ walletAvail, setWalletAvail ] = useState(false);
+    const [ provider, setProvider ] = useState<PhantomProvider | null>(null);
+    const [ connected, setConnected ] = useState(false);
+    const [ pubKey, setPubKey ] = useState<PublicKey | null>(null);
+
+
+    useEffect( ()=>{
+        if ("solana" in window) {
+            const solWindow = window as WindowWithSolana;
+            if (solWindow?.solana?.isPhantom) {
+                setProvider(solWindow.solana);
+                setWalletAvail(true);
+                // Attemp an eager connection
+                solWindow.solana.connect({ onlyIfTrusted: true });
+            }
+        }
+    }, []);
+
+    useEffect( () => {
+        provider?.on("connect", (publicKey: PublicKey)=>{ 
+            console.log(`connect event: ${publicKey}`);
+            setConnected(true); 
+            setPubKey(publicKey);
+        });
+        provider?.on("disconnect", ()=>{ 
+            console.log("disconnect event");
+            setConnected(false); 
+            setPubKey(null);
+        });
+
+    }, [provider]);
+
+
+    const connectHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+        console.log(`connect handler`);
+        provider?.connect()
+        .catch((err) => { console.error("connect ERROR:", err); });
+    }
+
+    const disconnectHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+        console.log("disconnect handler");
+        provider?.disconnect()
+        .catch((err) => {console.error("disconnect ERROR:", err); });
+    }
+    const IsPc = useMediaQuery({
+        query : "(min-width : 1024px)"
+    })
+
+    const walletButtonPc={
+        backgroundColor : '#00704A',
+        color: 'white',
+        textShadow : 'none',
+        width:'300px',
+        height:'50px',
+        
+        
+      }
+      const walletButtonMobile={
+        backgroundColor : 'white',
+        color: 'black',
+        textShadow : '5px 5px 5px gray'
+        
+        
+      }
+
+    return (
+        <div>
+            { walletAvail ?
+                <>
+                <button style={IsPc?walletButtonPc:walletButtonMobile} disabled={connected} onClick={connectHandler}>Connect Wallet</button>
+                 { connected ? <p>Your public key is : {pubKey?.toBase58()}</p> : null }
+                </>
+            :
+                <>
+                <button style={IsPc?walletButtonPc:walletButtonMobile} onClick={() => window.open('https://phantom.app/', '_blank')}>Connect Wallet</button>
+                
+                </>
+            }
+        </div>
+    );
+}
+
+export default Connect2Phantom;
---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import pi_logo from './pi_logo.png';
+import './header.css';
+import { NonceAccount } from '@solana/web3.js';
+import Connect2Phantom from './Connect2Phantom';
+import { useMediaQuery } from 'react-responsive';
+function Header() {
+
+  const buttonsPc={
+    display:'flex',
+    flex: 'row'
+  }
+  const buttonsMobile={
+    marginTop:'20px'
+  }
+  const IsPc = useMediaQuery({
+    query : "(min-width : 1024px)"
+})
+  return (
+    <div className="Header">
+        <img className="Header_img" src={pi_logo} alt='React' />  
+        <input/>
+        <div style={IsPc? buttonsPc:buttonsMobile as React.CSSProperties}>
+        <button>Explore</button>
+        <button>Minting</button>
+        <Connect2Phantom/>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
